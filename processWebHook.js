ed262628214aafb13fbbbb87924b2ff01ff55b87
Steamdiscoveryacademy <mike@steamdiscoveryacademy.com>
2021-03-01 13:22:25 -0600

    Click to ID working LIVE

--- processWebHook.js
+++ processWebHook.js
@@ -1,22 +1,31 @@
 import wixData from 'wix-data';
 
 $w.onReady(function () {
-	wixData.query("dsWebhookPayload")
-    .count()
-    .then( (num) => {
-      let numberOfItems = num;
-      let moreItems = numberOfItems - 4;
-      if (moreItems > 0) {
-        moreItems = 'plus ' + moreItems + ' additional items';
-        $w('#moreItems').text = moreItems;
-        $w('#moreItems').show();
-      }
-    } )
-    .catch( (error) => {
-      let errorMsg = error.message;
-      let code = error.code;
-    } );
-  });
+    wixData.query("dsWebhookPayload")
+        .count()
+        .then((num) => {
+            let numberOfItems = num;
+            let moreItems = numberOfItems - 4;
+            if (moreItems > 0) {
+                moreItems = 'plus ' + moreItems + ' additional items';
+                $w('#moreItems').text = moreItems;
+                $w('#moreItems').show();
+            }
+        })
+        .catch((error) => {
+            let errorMsg = error.message;
+            let code = error.code;
+        });
+});
 
-
-
+export function rptrTitle_click(event, $w) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here:
+    // console.log("rptrTitle_click: INERT");
+    let targetItem = $w("#dsWebhookPayload").getCurrentItem();
+    console.log("getCurrentItem: ");
+    console.log(targetItem);
+    let wixId = targetItem["_id"];
+    // let wixId = "testID";
+    $w('#thisKey').value = wixId;
+}

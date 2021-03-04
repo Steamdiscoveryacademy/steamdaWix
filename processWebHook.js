ef0857299b2da28e757f448e9c028442b4099b73
Steamdiscoveryacademy <mike@steamdiscoveryacademy.com>
2021-03-04 11:34:00 -0600

    long overdue, working well down to Course Confirmation

--- processWebHook.js
+++ processWebHook.js
@@ -5,23 +5,6 @@ const dashboardBaseUrl = 'https://manage.wix.com/dashboard/a8472b36-bc63-4063-bd
 const repeaterLimit = 10;
 
 $w.onReady(function () {
-    // wixData.query("dsWebhookPayload")
-    //     .count()
-    //     .then((num) => {
-    //         let numberOfItems = num;
-    //         let moreItems = numberOfItems - 10;
-    //         console.log("moreItems: " + moreItems);
-    //         if (moreItems > 0) {
-    //             moreItems = 'plus ' + moreItems + ' additional items';
-    //             $w('#moreItems').text = moreItems;
-    //             $w('#moreItems').show();
-    //         }
-    //     })
-    //     .catch((error) => {
-    //         let errorMsg = error.message;
-    //         let code = error.code;
-    //     });
-
     $w('#filterDescr').text = "Descriptison for All Un-Resolved Webhook's Received";
     $w('#dsWebhookPayload')
     $w('#moreItems').text = "moreItems Default";
@@ -39,6 +22,10 @@ export function rptrTitle_click(event, $w) {
     // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
     // Add your code for this event here:
     // console.log("rptrTitle_click: INERT");
+    let $item = $w.at(event.context);
+    // $item("#myRepeatedText").text = "Selected";
+    $item("#rptrButton").show();
+    //$item("#rptrLine").style.color = "#EA4335";
     let targetItem = $w("#dsWebhookPayload").getCurrentItem();
     // console.log("getCurrentItem: ");
     // console.log(targetItem);
@@ -138,10 +125,71 @@ export function dropdownFilter_change(event) {
 
 }
 
-export function btnApplicationSummer_click(event) {
+export function FormStack4223065_click(event, $w) {
     // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
     // Add your code for this event here:
     //?referralInfo=viewerNavigation
-    let url = dashboardBaseUrl + "blank-5?referralInfo=viewerNavigation&wixId=" + $w("#thisKey");
-    wixLocation.to(url);
+    // let url = dashboardBaseUrl + "blank-5?referralInfo=viewerNavigation&wixId=" + $w("#thisKey").value;
+    // console.log(url);
+    // console.log(dashboardBaseUrl);
+    // console.log("blank-5?referralInfo=viewerNavigation&wixId=");
+    // console.log($w("#thisKey").value);
+    // // wixLocation.to(url);
+    var objApplicationSummer = JSON.parse($w('#thisPayload').value);
+    let nameParent = objApplicationSummer.primary_parentguardian_name.first + ' ' + objApplicationSummer.primary_parentguardian_name.last;
+    $w('#nameParent').value = nameParent;
+    let emailParent = objApplicationSummer.primary_email_address;
+    //<Starts With Test>
+    // var atPosition = emailParent.lastIndexOf("@");
+    // emailParent = emailParent.substr(0, atPosition)
+    //</Starts With Test>
+    // let emailParent = "abbytammen@gmail.com";//TEST: FAIL
+    // let emailParent = "goodphyte@gmail.com";//TEST: FAIL
+    // let emailParent = "lisel@steamda.com";//TEST: FAIL
+    $w('#emailParent').value = emailParent;
+    let phone = objApplicationSummer.primary_mobile_phone;
+    // let phone = "(434) 825-2508";
+    let pattern = /[^0-9]/g;
+    let phoneRaw = phone.replace(pattern, '');
+    $w('#phoneParent').value = phoneRaw;
+
+    let filter1 = wixData.filter().eq("mainPhone", phoneRaw);
+    let filter2 = wixData.filter().startsWith("loginEmail", emailParent);
+    // let filter1 = wixData.filter().eq("lastName", "Pritchard");
+    // let filter2 = wixData.filter().eq("firstName", "Lisel");
+    // let finalFilter = filter1.or(filter2);
+    let finalFilter = filter2;
+    $w("#dsMembers").setFilter(finalFilter);
+    $w("#btnAssignMember").show();
+}
+
+export function btnAssignMember_click(event) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here:
+    $w("#thisMemberId").value = "dc74d778-c930-4cc5-8bd8-99f314b0a4c7";
+    $w("#btnConfirmClasses").show();
+}
+
+export function btnConfirmClasses_click(event) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here:
+    $w("#memberIdParent").value = $w("#thisMemberId").value;
+    var objApplicationSummer = JSON.parse($w('#thisPayload').value);
+    let gradeStudent = objApplicationSummer.grade_during_the_202021_school_year.value + 0;
+    let dobStudent = objApplicationSummer.date_of_birth;
+    let gradeLevelStudnet = gradeLeveFromGrade(gradeStudent);
+    $w("#dobStudent").value = dobStudent;
+    $w("#nameParentCC").value = $w("#nameParent").value;
+    $w("#emailParentCC").value = $w("#nameParent").value;
+    $w("#phoneParentCC").value = "(" + $w("#phoneParent").value.substr(0, 3) + ") "
+        + $w("#phoneParent").value.substr(3, 3) + "-";
+    + $w("#phoneParent").value.substr(6);
+    // $w("#");
+}
+
+export function validateGradeVsAge(currentGrade, DOB) {
+    return Number.random * 10 > 5 ? true : false;
+}
+export function gradeLeveFromGrade(currentGrade) {
+    return Number.random * 10 > 5 ? true : false;
 }

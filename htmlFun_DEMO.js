dace2f4baeaeb00e897946ba3307efd1ebbef090
Steamdiscoveryacademy <mike@steamdiscoveryacademy.com>
2021-03-06 17:52:31 -0600

    Thru 3

--- htmlFun_DEMO.js
+++ htmlFun_DEMO.js
@@ -8,11 +8,18 @@ export function htmlFunButton_click(event) {
         return false;
     }
     //</Un-Comment one or the Other to Enable/Disable thid DEMO>
-
+    let weeksCount = 9 - Math.floor(Math.random() - 4)
     // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
     // Add your code for this event here:
     let classesWeekOne = Math.floor(Math.random() * 4)
     let classesWeekTwo = Math.floor(Math.random() * 4)
+    let classesWeekThree = Math.floor(Math.random() * 4)
+    let classesWeekFour = Math.floor(Math.random() * 4)
+    let classesWeekFive = Math.floor(Math.random() * 4)
+    let classesWeekSix = Math.floor(Math.random() * 4)
+    let classesWeekSeven = Math.floor(Math.random() * 4)
+    let classesWeekEight = Math.floor(Math.random() * 4)
+    let classesWeekNine = Math.floor(Math.random() * 4)
     let dlStyle = "\"font-size: 16px; font-family : 'Avenir Black'; background-color: yellow;\"";
     let dhStyle = "color: blue; text-decoration: underline; line-height: 1";
     let ddStyle = "color: green; text-indent: 55px;";
@@ -40,7 +47,11 @@ export function htmlFunButton_click(event) {
         }
         textWeek1 += "</dl>"
             ;
-    $w("#textWeek1").html = textWeek1;
+    if (weeksCount >= 1) {
+        $w("#textWeek1").html = textWeek1;
+    } else {
+        $w("#textWeek1").hide();
+    }
     //</Week 1>
 
     //<Week 2>
@@ -48,28 +59,60 @@ export function htmlFunButton_click(event) {
     $w("#switch202").hide();
     $w("#switch203").hide();
     let textWeek2 = "<dl style=" + dlStyle + ">"
-        + "<dh style='" + dhStyle + "'>Week 2: June 14-18 2021</dh>";
-        if (classesWeekTwo >= 1) {
-            textWeek2 += "<dd style='" + ddStyle + "'>From Ada Lovelace to Grace Hopper</li></dd>";
-            $w("#switch201").show();
-            $w("#switch201").checked = false;
-        }
-        if (classesWeekTwo >= 2) {
-            textWeek2 += "<dd style='" + ddStyle + "'>Playing Bongo's Like Richard Feynman</li></dd>";
-            $w("#switch202").show();
-            $w("#switch202").checked = false;
-        }
-        if (classesWeekTwo >= 3) {
-            textWeek2 += "<dd style='" + ddStyle + "'>The Eels and the Multiverse Theory</li></dd>";
-            $w("#switch203").show();
-            $w("#switch203").checked = false;
-        }
-        textWeek2 += "</dl>"
-            ;
-    $w("#textWeek2").html = textWeek2;
+    + "<dh style='" + dhStyle + "'>Week 2: June 14-18 2021</dh>";
+    if (classesWeekTwo >= 1) {
+        textWeek2 += "<dd style='" + ddStyle + "'>From Ada Lovelace to Grace Hopper</li></dd>";
+        $w("#switch201").show();
+        $w("#switch201").checked = false;
+    }
+    if (classesWeekTwo >= 2) {
+        textWeek2 += "<dd style='" + ddStyle + "'>Playing Bongo's Like Richard Feynman</li></dd>";
+        $w("#switch202").show();
+        $w("#switch202").checked = false;
+    }
+    if (classesWeekTwo >= 3) {
+        textWeek2 += "<dd style='" + ddStyle + "'>The Eels and the Multiverse Theory</li></dd>";
+        $w("#switch203").show();
+        $w("#switch203").checked = false;
+    }
+    textWeek2 += "</dl>"
+    ;
+    if (weeksCount >= 2) {
+        $w("#textWeek2").html = textWeek2;
+    } else {
+        $w("#textWeek2").hide();
+    }
     //</Week 2>
 
-
+    //<Week 3>
+    $w("#switch301").hide();
+    $w("#switch302").hide();
+    $w("#switch303").hide();
+    let textWeek3 = "<dl style=" + dlStyle + ">"
+        + "<dh style='" + dhStyle + "'>Week 3: June 14-18 2021</dh>";
+    if (classesWeekThree >= 1) {
+        textWeek3 += "<dd style='" + ddStyle + "'>From Ada Lovelace to Grace Hopper</li></dd>";
+        $w("#switch301").show();
+        $w("#switch301").checked = false;
+    }
+    if (classesWeekThree >= 2) {
+        textWeek2 += "<dd style='" + ddStyle + "'>Playing Bongo's Like Richard Feynman</li></dd>";
+        $w("#switch302").show();
+        $w("#switch302").checked = false;
+    }
+    if (classesWeekThree >= 3) {
+        textWeek2 += "<dd style='" + ddStyle + "'>The Eels and the Multiverse Theory</li></dd>";
+        $w("#switch303").show();
+        $w("#switch303").checked = false;
+    }
+    textWeek3 += "</dl>"
+        ;
+    if (weeksCount >= 3) {
+        $w("#textWeek3").html = textWeek3;
+    } else {
+        $w("#textWeek3").hide();
+    }
+    //</Week 3>
 
     // $w("#htmlFunAlert").value = "Applying the almost unused HTML construct called 'Data Lists' ('<dl>') to the simple Camping List Text to the Dynamic Content for Weeks 1 & 2 of Steam Discovery Academy";
 }

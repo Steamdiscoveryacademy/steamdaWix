478d5bd9a7f2db57dcd94d0d629d023276096527
ferdely <bradlowry@gmail.com>
2021-06-07 13:51:20 -0500

    Steps LoopSwitch

--- agile/sprint/finishLine/instantiateLoopSwitchEnrollmentSteps.js
+++ agile/sprint/finishLine/instantiateLoopSwitchEnrollmentSteps.js
@@ -1,12 +1,13 @@
+// ! Get stepArrayOrig from overallEnrollmentStepLoopAutoDox.js
 
 // ø <---------- <instantiateLoopSwitchEnrollmentSteps>  ---------->
 export function instantiateLoopSwitchEnrollmentSteps(){
+    // let stepArrayOrig = [ 'IINSTANTIATE','PREP_ppMember','EXECUTE_ppMember','PREP_stMember','EXECUTE_stMember','PREP_ppContact','PREP_ppDatabase','PREP_stContact','PREP_stDatabase','PREP_spContact','PREP_spDatabase','EXECUTE_ppContact','EXECUTE_ppDatabase','EXECUTE_stContact','EXECUTE_stDatabase','EXECUTE_spContact','EXECUTE_spDatabase','CCOMPLETE' ];
     let stepArrayOrig = [ 'IINSTANTIATE','PREP_ppMember','EXECUTE_ppMember','PREP_stMember','EXECUTE_stMember','PREP_ppContact','PREP_ppDatabase','PREP_stContact','PREP_stDatabase','PREP_spContact','PREP_spDatabase','EXECUTE_ppContact','EXECUTE_ppDatabase','EXECUTE_stContact','EXECUTE_stDatabase','EXECUTE_spContact','EXECUTE_spDatabase','CCOMPLETE' ];
-    
-	memory.setItem('enrollmentStepList','IINSTANTIATE,PREP_ppMember,EXECUTE_ppMember,PREP_ppContact,EXECUTE_ppContact,PREP_ppDatabase,EXECUTE_ppDatabase,PREP_stMember,EXECUTE_stMember,PREP_stContact,EXECUTE_stContact,PREP_stDatabase,EXECUTE_stDatabase,PREP_spContact,EXECUTE_spContact,PREP_spDatabase,EXECUTE_spDatabase,CCOMPLETE');
+    let stepList = stepArrayOrig.toString();
+	memory.setItem('enrollmentStepList',stepList);
 	memory.setItem('enrollmentStepCompleted','NNOT_AAPPLICABLE');
 	memory.setItem('enrollmentStepCurrent',stepArrayOrig[0]);
-	// let stepNext = stepArrayOrig[1];
 	memory.setItem('enrollmentStepNext',stepArrayOrig[1]);
 }
 // ø <---------- </instantiateLoopSwitchEnrollmentSteps> ---------->

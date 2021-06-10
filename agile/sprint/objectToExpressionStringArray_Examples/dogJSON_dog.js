55f29866c345f961c735c8d4b8377c8be3d4e98a
ferdely <bradlowry@gmail.com>
2021-06-10 09:18:11 -0500

    commit

---
+++ agile/sprint/objectToExpressionStringArray_Examples/dogJSON_dog.js
@@ -0,0 +1,11 @@
+let PARAM_dogObject =  { name: 'Chester', age: 14 }
+let PARAM_dogJSON =  `{"name":"Chester","age":14}`;
+
+// ! <Sylvester McMonkey McBean>
+
+let dogObject = {};
+dogObject.name = "Chester";
+dogObject.age = 14;
+
+console.warn('dogObject: ');
+console.warn(dogObject);

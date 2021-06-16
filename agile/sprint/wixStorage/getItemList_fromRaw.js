511b80ceda12a495422fa4f08a3a7bf7c014e1aa
ferdely <bradlowry@gmail.com>
2021-06-16 17:41:58 -0500

    organizing and cleaning up storage.getItem() data

---
+++ agile/sprint/wixStorage/getItemList_fromRaw.js
@@ -0,0 +1,166 @@
+// ! <RAW Object Code>
+let storageObject = {};
+storageObject.storageObjectArray = [];
+let storageObjectElement = {};
+let sourseString = '// @source:';
+// ø <wixStorageCleanup>
+let wixStorageCleanup = false;
+if(wixStorageCleanup){
+    sourseString = '// @source: wixStorageCleanup';
+
+    console.warn('storageObject.storageObjectArray: ');
+    console.warn(storageObject.storageObjectArray);
+}
+// ø </wixStorageCleanup>
+// ø <wixStorageCleanup_MergeFunctionStorage>
+let wixStorageCleanup_MergeFunctionStorage = false;
+if(wixStorageCleanup_MergeFunctionStorage){
+    sourseString = '// @source: wixStorageCleanup_MergeFunctionStorage';
+
+    console.warn('storageObject.storageObjectArray: ');
+    console.warn(storageObject.storageObjectArray);
+}
+// ø </wixStorageCleanup_MergeFunctionStorage>
+// ø <storageObject_CleanUpCode_20210607215000>
+let storageObject_CleanUpCode_20210607215000 = true;
+if(storageObject_CleanUpCode_20210607215000){
+    sourseString = '// @source: storageObject_CleanUpCode_20210607215000';
+
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'ppMemberPrepJSON';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'ppMemberExecuteUpsert';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'stMemberPrepJSON';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'stMemberExecuteUpsert';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'ppContactPrepJSON';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'ppDatabasePrepJSON';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'stContactPrepJSON';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'stDatabasePrepJSON';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'spContactPrepJSON';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'spDatabasePrepJSON';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'ppContactExecuteUpsert';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'ppDatabaseExecuteUpsert';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'stContactExecuteUpsert';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'stDatabaseExecuteUpsert';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'spContactExecuteUpsert';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+    
+    storageObjectElement = {};
+    storageObjectElement.memoryKey = 'spDatabaseExecuteUpsert';
+    storageObjectElement.memoryKind = 'memory';
+    storageObjectElement.codeKind = '|HHOLDER|CURRENT|NEW|';
+    storageObjectElement.confirmed = true;
+    storageObject.storageObjectArray.push(storageObjectElement);
+
+    // console.warn('storageObject.storageObjectArray: ');
+    // console.warn(storageObject.storageObjectArray);
+}
+// ø </storageObject_CleanUpCode_20210607215000>
+// ! </RAW Object Code>
+
+let sourceArray = storageObject.storageObjectArray;
+// let sourceArray = [];
+
+let getItemStringArray = [];
+let element = {};
+let getItemStringThis = '';
+
+for (let index = 0; index < sourceArray.length; index++) {
+    element = sourceArray[index];
+
+    getItemStringThis = element.memoryKind;
+    getItemStringThis += '.getItem(';
+    getItemStringThis += element.memoryKey;
+    getItemStringThis += ')';
+    getItemStringArray.push(getItemStringThis);
+}
+
+console.warn('//getItemStringArray: ');
+console.warn(sourseString);
+console.warn(getItemStringArray);

6c734b14108fb3b6f0178e8734abaf9925f091b9
ferdely <bradlowry@gmail.com>
2021-08-30 07:09:19 -0500

    getContactByEmailAndNotID front end

--- agile/sprint/frontEnd/getContactByEmailAndNotIdFunction_FrontEnd.js
+++ agile/sprint/frontEnd/getContactByEmailAndNotIdFunction_FrontEnd.js
@@ -1,5 +1,5 @@
 // ø <---------- <getContactByEmailAndNotIdFunction Front-End>  ---------->
-export async function getContactByEmailAndNotIdFunction(emailToFind = 'invalid Email',notIdToFind = 'invalid Id', diagnosticOnly = false) {
+export async function getContactByEmailAndNotIdFunction(emailToFind = 'invalid Email', notIdToFind = 'invalid Id', diagnosticOnly = false) {
     let isValid = emailToFind.indexOf('@') > 0 ? true : false;
     isValid = notIdToFind.length !== 36 ? false : isValid;
     let logString = '';
@@ -7,23 +7,22 @@ export async function getContactByEmailAndNotIdFunction(emailToFind = 'invalid E
     logString += `\nnotIdToFind: ${notIdToFind}`;
     if (!isValid) {
         logString = 'One of the two following Parameter Values is InValid:' + logString;
-        
+
         local.setItem('logString', logString);
         return;
     }
     logString = 'For the following Parameters:' + logString;
-    
-	let queryRresults = await steamdaGetContactByEmailAndNotIdFunction(emailToFind,notIdToFind);
+
+    let queryRresults = await steamdaGetContactByEmailAndNotIdFunction(emailToFind, notIdToFind);
     let count = queryRresults.resultsCount;
-    
+
     logString += `\nthe Query of Contacts for \nPrimary Email Equal to: '${emailToFind}' \nAND Contact Primary Email Not-Equal to: ${notIdToFind}`;
     logString += `\nReturned:\n`;
     logString += `BEGIN queryRresults:\n`;
-    logString += JSON.stringify(queryRresults,undefined,4);
-    // logString += queryRresults;
+    logString += JSON.stringify(queryRresults, undefined, 4);
     logString += `\nEND queryRresults\n`;
     if (count > 1) {
-        local.setItem('superEnrollmentStatus','ALERT');
+        local.setItem('superEnrollmentStatus', 'ALERT');
         logString += `\nThe Count is More Than One [${count}] this is a Serious Probelem, for this reason the 'superEnrollmentStatus' has been set to 'ALERT' and no further Action taken.`;
         local.setItem('logString', logString);
         return;
@@ -33,23 +32,28 @@ export async function getContactByEmailAndNotIdFunction(emailToFind = 'invalid E
         local.setItem('logString', logString);
         return;
     }
-    let idToDelete = queryRresults.results._items[0]._id//GUESS, needs verification
+    let idToDelete = queryRresults.results._items[0]._id
+    //verificatio:   queryRresults.results._items[0]._id
     if (count === 1) {
         logString += `\nThe Count is One, this is the BUG exist in the form of the Contact with ID: ${idToDelete}`;
         logString += `\ndiagnosticOnly: ${diagnosticOnly}: Meaning the found Contact will `;
         logString += diagnosticOnly ? 'NOT ' : '';
         logString += `be Deleted at this time`;
-        if(diagnosticOnly){
+        if (diagnosticOnly) {
             local.setItem('logString', logString);
             return;
         }
     }
-    
+
     // ø <Delete the BUG Contact>
-    logString += `\n\nThe code to actually Delete Contact[${idToDelete}] is not ready yet...`;
+    logString += `\n\nThe code to actually Delete Contact[${idToDelete}] would look like this:`;
+    logString += `\nlet deleteResults = await steamdaDeleteContactById('${idToDelete}'')`;
+    let deleteResults = await steamdaDeleteContactById(idToDelete)
+    logString += `\n\ndeleteResults:\n==============\n`;
+    logString += deleteResults;
     // ø </Delete the BUG Contact>
     local.setItem('logString', logString);
-    
+
     return;
 }
 // ø <---------- </getContactByEmailAndNotIdFunction Front-End> ---------->

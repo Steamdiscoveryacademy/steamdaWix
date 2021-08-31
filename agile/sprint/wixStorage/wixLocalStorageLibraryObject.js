8b0b4a1ea4d14104c534250f92168f9351a537e6
ferdely <bradlowry@gmail.com>
2021-08-31 08:05:22 -0500

    added attribute to local library

--- agile/sprint/wixStorage/wixLocalStorageLibraryObject.js
+++ agile/sprint/wixStorage/wixLocalStorageLibraryObject.js
@@ -1,9 +1,14 @@
+let DOX = 'HOLDER';
 let wixLocalStorageLibraryObject = {
     "admin": {
         "title": "WiX-Storage 'Library' for Post-Enrollment for Steam DA",
         "descr": "a longer descr",
-        "localAttributesDoxPPEQArray": ["2021-08-29T23:59:59|collected _all_ from postEnrollment.js as baseline"],
-        "notes": []
+        "localAttributesDoxPPEQArray": ["2021-08-29T23:59:59|Baseline|collected _all_ from postEnrollment.js as baseline"],
+        "localAttributesNotes": [],
+        "sessionAttributesDoxPPEQArray": ["2021-08-29T23:59:59|Baseline|none so far"],
+        "sessionAttributesNotes": [],
+        "memoryAttributesDoxPPEQArray": ["2021-08-29T23:59:59|Baseline|not collected yet"],
+        "memoryAttributesNotes": ["there are many to be collected for baseline, but postponed"],
     },
     "wixLocalStorageArray": [
         "comboName",
@@ -66,6 +71,9 @@ let wixLocalStorageLibraryObject = {
     ]
 };
 
+DOX = '2021-08-30T07:49:01|stepMessagingJSON|the result of all the PPEQ Append Calls for a Step|maybe for bug, maybe long-term';
+wixLocalStorageLibraryObject.admin.localAttributesDoxPPEQArray.push(DOX)
+wixLocalStorageLibraryObject.wixLocalStorageArray.push('stepMessagingJSON')
 
 
 

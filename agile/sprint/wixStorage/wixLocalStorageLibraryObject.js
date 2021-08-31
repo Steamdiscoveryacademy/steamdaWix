6ece7823a4dd78781b6e7d1588e911d22fd0c507
ferdely <bradlowry@gmail.com>
2021-08-31 07:44:39 -0500

    rename and enhance

---
+++ agile/sprint/wixStorage/wixLocalStorageLibraryObject.js
@@ -0,0 +1,73 @@
+let wixLocalStorageLibraryObject = {
+    "admin": {
+        "title": "WiX-Storage 'Library' for Post-Enrollment for Steam DA",
+        "descr": "a longer descr",
+        "localAttributesDoxPPEQArray": ["2021-08-29T23:59:59|collected _all_ from postEnrollment.js as baseline"],
+        "notes": []
+    },
+    "wixLocalStorageArray": [
+        "comboName",
+        "enrollmentStepCompletedListAll",
+        "enrollmentStepListAll",
+        "familyEmail",
+        "familyId",
+        "familySeed",
+        "kAppendString",
+        "lastErrorString",
+        "lastParamObject",
+        "lastResponseObject",
+        "loopExitAfterStep",
+        "loopExitNow",
+        "ondeckEnrollmentJSON",
+        "ppFirst",
+        "ppLast",
+        "secondaryEmail",
+        "secondaryId",
+        "spFirst",
+        "spLast",
+        "staffIdentifiedFamilyId",
+        "stFirst",
+        "stLast",
+        "stPreferredFirst",
+        "studentEmail",
+        "studentId",
+        "sufficientWatchdogBootstrapKeyArray",
+        "superEnrollmentStatus",
+        "termBeginMMDD",
+        "termEndMMDD",
+        "termEndYYYYMMDD",
+        "termId",
+        "termLabelKey",
+        "timezoneOffset",
+        "tzAbbrv",
+        "uiStDobString",
+        "uniqueWatchdogBootstrapKeyArray",
+        "webhookThisResolved",
+        "webhookThisStatus",
+        "weekIdToLabelKeyJSON",
+        "wixWebhookStatus",
+        "wixWebhookId",
+        "yyyymm"
+    ],
+    "wixLocalStorageArrayPRETRASH": [
+        "famiZZZlyId",
+        "famiZZZlySeed",
+        "lastZZZErrorString",
+        "lastZZZParamObject",
+        "lastZZZResponseObject",
+        "loopZZZExitNow",
+        "ondeZZZckEnrollmentJSON",
+        "timeZZZoneOffset",
+        "webhZZZookThisResolved",
+        "webhZZZookThisStatus",
+        "wixWZZZebhookId",
+        "wixWZZZebhookStatus",
+        "yyyyZZZmm"
+    ]
+};
+
+
+
+
+console.warn("wixLocalStorageLibraryObject: ");
+console.warn(wixLocalStorageLibraryObject);

a3273b36e24582a18c986a6dafd4f08ab7e5b1dd
ferdely <bradlowry@gmail.com>
2021-09-03 14:52:24 -0500

    rename stuff parsePPEQ_toObjectArraysByKey

---
+++ agile/sprint/coreLoopSwitchParadigm/stepLogString_paradigm/parsePPEQ_toObjectArraysByKey/2021-09-03T14:48:01_parsePPEQ_toObjectArrayByKey_CHOP.js
@@ -0,0 +1,71 @@
+// ø <---------- <parsePPEQ_toObjectArraysByKey UTILITY>  ---------->
+export function parsePPEQ_toObjectArraysByKey(ppeqString = 'STRING'){
+    // pstEnrSeven202108UTILITY SHORT
+    let accordingToSufficientBootstrapWatchdog = "not Necessary, parses to ObjectArrayByKey (objects), with bootstrap-watchdog of 'NA' if no match";
+    let sufficientObjectLookup = { EMERGENCY: { bootstrap: 'DANGER', watchdog: 'EMERGENCY' },CRITICAL: { bootstrap: 'DANGER', watchdog: 'CRITICAL' },ERROR: { bootstrap: 'DANGER', watchdog: 'ERROR' },DANGER: { bootstrap: 'DANGER', watchdog: 'ERROR' },ALERT: { bootstrap: 'WARNING', watchdog: 'ALERT' },WARNING: { bootstrap: 'WARNING', watchdog: 'WARNING' },NOTICE: { bootstrap: 'WARNING', watchdog: 'NOTICE' },SUCCESS: { bootstrap: 'SUCCESS', watchdog: 'INFO' },PRIMARY: { bootstrap: 'PRIMARY', watchdog: 'INFO' },INFO: { bootstrap: 'INFO', watchdog: 'INFO' },SECONDARY: { bootstrap: 'SECONDARY', watchdog: 'INFO' },DEVEL: { bootstrap: 'DEVEL', watchdog: 'DEBUG' },DEBUG: { bootstrap: 'DEVEL', watchdog: 'DEBUG' } };
+    let sufficientKeyArray = Object.keys(sufficientObjectLookup);
+    let responseObjectArraysByKey = {};
+    let elementArray = {};
+    let elementObject = {};
+    let lookupObject = {};
+    let key = 'STRING';
+    let holder = 'STRING';
+    let index = 0;
+    
+    ppeqString = ppeqString.substr(0, ppeqString.length - 1);
+    
+    let ppeqElementArray = ppeqString.split('|');
+    ppeqElementArray.forEach(elementString => {
+        elementArray = elementString.split('=');
+        key = elementArray[0];
+        key = key.toUpperCase();
+        if(!Object.keys(responseObjectArraysByKey).includes(key)){
+            responseObjectArraysByKey[key] = [];
+        }
+        if(sufficientKeyArray.includes(key)){
+            lookupObject = sufficientObjectLookup[key];
+        }else{
+            lookupObject = { bootstrap: 'NA', watchdog: 'NA' }
+        }
+        elementObject = {};
+        elementObject.message = elementArray[1];
+        elementObject.key = key;
+        holder = typeof elementArray[2] === 'string' ? elementArray[2] : '';
+        elementObject.line = elementArray[2];
+        elementObject.bootstrap = lookupObject.bootstrap;
+        elementObject.watchdog = lookupObject.watchdog;
+        holder = typeof elementArray[4] === 'string' ? elementArray[4] : '';
+        elementObject.postLogString = holder;
+        elementObject.postLog = elementObject.postLogString === 'TTRUE' ? true : false;
+        elementObject.index = index;
+        elementObject._id = index.toString();
+        responseObjectArraysByKey[key].push(elementObject);
+        index++;
+    });  
+    return responseObjectArraysByKey;
+}
+// ø <---------- </parsePPEQ_toObjectArraysByKey UTILITY> ---------->
+
+
+let paramStepLogString = "PENDING";
+paramStepLogString = "info=The 'IINSTANTIATE' Step has an Info Override UI==FFALSE=stateInstantiate=IINSTANTIATE|danger=danger in end of ui force==FFALSE=stateInstantiate=IINSTANTIATE|";
+
+let messageObjectArraysByKey = parsePPEQ_toObjectArraysByKey(paramStepLogString);
+
+console.warn('paramStepLogString: ');
+let paramStepLogStringArray = paramStepLogString.split('|');
+let paramStepLogStringDisplay = '';
+paramStepLogStringArray.forEach(element => {
+    // console.log(element);
+    paramStepLogStringDisplay += element + '|\n';
+});
+
+console.warn(paramStepLogStringDisplay);
+
+
+console.warn('messageObjectArraysByKey: ');
+console.warn(messageObjectArraysByKey);
+console.warn(JSON.stringify(messageObjectArraysByKey,undefined,4));
+
+
+

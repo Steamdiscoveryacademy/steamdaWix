622f985fe41401ec70d63f514a59e381528e5997
ferdely <bradlowry@gmail.com>
2021-09-26 08:12:23 -0500

    Backend Code/macAliases/YAML-files

--- backend/099_README/customRewriteForMulitplyFunction.js
+++ backend/099_README/customRewriteForMulitplyFunction.js
@@ -10,8 +10,10 @@ export function multiply(factor1, factor2) {
 // }
 export function multiplyFor_BACKEND_FILE_NAME(factor1, factor2) {
     // return factor1 * factor2;
+    let thisMultiplyJSON = 'the JSON from the BACKEND FILE FOLDER YAML'
+    session.setItem('lastMultiplyJSON',thisMultiplyJSON)
     let product =  factor1 * factor2;
-    return `In SITE_NAME Backend, in the file 'BACKEND_FILE_NAME', the multiply() function return a product of ${product}`
+    return `In SITE_NAME Backend, in the file 'BACKEND_FILE_NAME', the multiply() function return a product of ${product} | [see session.getItem('lastMultiplyJSON') for YAML to JSON for this backend file]`
 }
 // ¡ </REWRITE - Make the function a the Same BUT Unique for each Backend FIle>
 

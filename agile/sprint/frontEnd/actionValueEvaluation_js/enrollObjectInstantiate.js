094c5ae27ecdf4523420c6c27dcbb8d809540408
ferdely <bradlowry@gmail.com>
2021-10-01 10:08:15 -0500

    commit

--- agile/sprint/frontEnd/actionValueEvaluation_js/enrollObjectInstantiate.js
+++ agile/sprint/frontEnd/actionValueEvaluation_js/enrollObjectInstantiate.js
@@ -1,51 +1,89 @@
 export async function instantiateSimpleDemogfxObject() {
     // ø <final in Post-Enrollment>
     // let enroll = {}
-    // let simpleDemogfxObject = {}
-    // simpleDemogfxObject.termId = local.getItem('termId')
-    // simpleDemogfxObject.staffIdentifiedFamilyId = local.getItem('staffIdentifiedFamilyId')
-    // simpleDemogfxObject.familyIdStatus = 'Staff-Eye-D'
-    // simpleDemogfxObject.familySeed = local.getItem('familySeed')
-    // simpleDemogfxObject.primary = {}
-    // simpleDemogfxObject.primary.familyId = local.getItem('familyId')
-    // simpleDemogfxObject.primary.familyEmail = local.getItem('familyEmail')
-    // simpleDemogfxObject.primary.ppFirst = local.getItem('ppFirst')
-    // simpleDemogfxObject.primary.ppLast = local.getItem('ppLast')
-    // simpleDemogfxObject.student = {}
-    // simpleDemogfxObject.student.studentId = local.getItem('studentId')
-    // simpleDemogfxObject.student.studentEmail = local.getItem('studentEmail')
-    // simpleDemogfxObject.student.stFirst = local.getItem('stFirst')
-    // simpleDemogfxObject.student.stPreferredFirst = local.getItem('stPreferredFirst')
-    // simpleDemogfxObject.student.stLast = local.getItem('stLast')
-    // simpleDemogfxObject.secondary = {}
-    // simpleDemogfxObject.secondary.secondaryId = local.getItem('secondaryId')
-    // simpleDemogfxObject.secondary.secondaryEmail = local.getItem('secondaryEmail')
-    // simpleDemogfxObject.secondary.spFirst = local.getItem('spFirst')
-    // simpleDemogfxObject.secondary.spLast = local.getItem('spLast')
+    // enroll.familyId = PENDING
+    // enroll.familyIdStatus = 'PENDING'//'Staff-Eye-D' inferred by Data, precision here not necessary
+    // enroll.maxPreviousFamilyTermId = 201506
+    // enroll.maxPreviousStudentTermId = 201506
+    // enroll.wixWebhookId = local.getItem('wixWebhookId')
+    // ø <demogrfx-application-processed>
+    // enroll.application = {}
+    // enroll.application.termId = local.getItem('termId')
+    // enroll.application.staffIdentifiedFamilyId = local.getItem('staffIdentifiedFamilyId')
+    // enroll.application.familySeed = local.getItem('familySeed')
+    // enroll.application.primary = {}
+    // enroll.application.primary.familyId = local.getItem('familyId')
+    // enroll.application.primary.familyEmail = local.getItem('familyEmail')
+    // enroll.application.primary.ppFirst = local.getItem('ppFirst')
+    // enroll.application.primary.ppLast = local.getItem('ppLast')
+    // enroll.application.student = {}
+    // enroll.application.student.studentId = local.getItem('studentId')
+    // enroll.application.student.studentEmail = local.getItem('studentEmail')
+    // enroll.application.student.stFirst = local.getItem('stFirst')
+    // enroll.application.student.stPreferredFirst = local.getItem('stPreferredFirst')
+    // enroll.application.student.stLast = local.getItem('stLast')
+    // enroll.application.secondary = {}
+    // enroll.application.secondary.secondaryId = local.getItem('secondaryId')
+    // enroll.application.secondary.secondaryEmail = local.getItem('secondaryEmail')
+    // enroll.application.secondary.spFirst = local.getItem('spFirst')
+    // enroll.application.secondary.spLast = local.getItem('spLast')
     // ø </final in Post-Enrollment>
-
-    // ø <TESTING>
+    // ø </demogrfx-application-processed>
+    
+    
+    
     let enroll = {}
-    let simpleDemogfxObject = {}
-    simpleDemogfxObject.termId = local.getItem('termId')
-    simpleDemogfxObject.staffIdentifiedFamilyId = '2297c168-cc91-43d1-8864-eb1182967d35'
-    simpleDemogfxObject.familyIdStatus = 'Staff-Eye-D'
-    simpleDemogfxObject.familySeed = '2297c168cc9143d18864eb1182967d35'
-    simpleDemogfxObject.primary = {}
-    simpleDemogfxObject.primary.familyId = ''
-    simpleDemogfxObject.primary.familyEmail = 'jessikazmuda@gmail.com'
-    simpleDemogfxObject.primary.ppFirst = 'Jessika'
-    simpleDemogfxObject.primary.ppLast = 'Bottiani'
-    simpleDemogfxObject.student = {}
-    simpleDemogfxObject.student.studentId = 'INSTANTIATE'
-    simpleDemogfxObject.student.studentEmail = 'steamdiscoveryacademy+grayson2297@gmail.com'
-    simpleDemogfxObject.student.stFirst = 'Grayson'
-    simpleDemogfxObject.student.stPreferredFirst = 'Grayson'
-    simpleDemogfxObject.student.stLast = 'Wilkinson'
-    simpleDemogfxObject.secondary = {}
-    simpleDemogfxObject.secondary.secondaryId = 'EEMPTY'
-    simpleDemogfxObject.secondary.secondaryEmail = 'eempty'
-    simpleDemogfxObject.secondary.spFirst = 'James'
-    simpleDemogfxObject.secondary.spLast = 'Bottiani'
+    enroll.familyId = PENDING
+    enroll.familyIdStatus = 'PENDING'//'Staff-Eye-D' inferred by Data, precision here not necessary
+    enroll.maxPreviousFamilyTermId = 201506
+    enroll.maxPreviousStudentTermId = 201506
+    enroll.wixWebhookId = local.getItem('wixWebhookId')
+    // ø <demogrfx-application-processed>
+    // ø <TESTING>
+    enroll.application = {}
+    enroll.application.termId = local.getItem('termId')
+    enroll.application.staffIdentifiedFamilyId = '2297c168-cc91-43d1-8864-eb1182967d35'
+    enroll.application.familySeed = '2297c168cc9143d18864eb1182967d35'
+    enroll.application.primary = {}
+    enroll.application.primary.familyId = ''
+    enroll.application.primary.familyEmail = 'jessikazmuda@gmail.com'
+    enroll.application.primary.ppFirst = 'Jessika'
+    enroll.application.primary.ppLast = 'Bottiani'
+    enroll.application.student = {}
+    enroll.application.student.studentId = 'INSTANTIATE'
+    enroll.application.student.studentEmail = 'steamdiscoveryacademy+grayson2297@gmail.com'
+    enroll.application.student.stFirst = 'Grayson'
+    enroll.application.student.stPreferredFirst = 'Grayson'
+    enroll.application.student.stLast = 'Wilkinson'
+    enroll.application.secondary = {}
+    enroll.application.secondary.secondaryId = 'EEMPTY'
+    enroll.application.secondary.secondaryEmail = 'eempty'
+    enroll.application.secondary.spFirst = 'James'
+    enroll.application.secondary.spLast = 'Bottiani'
     // ø </TESTING>
+    // ø </demogrfx-application-processed>
+    
+    // ø <current-evaluation-personDbase-personData-wixWebhookId>
+    enroll.personData = {}
+    // ø </current-evaluation-personDbase-personData-wixWebhookId>
+    
+    // ø <contacts-directQuery-emailQuery-emailQueryWithKludge>
+    enroll.emailQuery = {}
+    // ø </contacts-directQuery-emailQuery-emailQueryWithKludge>
+    
+    // ø <action-actionEvaluation-final>
+    enroll.action = {}
+    enroll.action.primary = {}
+    enroll.action.primary.member = PENDING
+    enroll.action.primary.contact = PENDING
+    enroll.action.primary.dataBase = PENDING
+    enroll.action.student.member = PENDING
+    enroll.action.student.contact = PENDING
+    enroll.action.student.dataBase = PENDING
+    enroll.action.secondary.member = NA
+    enroll.action.secondary.contact = PENDING
+    enroll.action.secondary.dataBase = PENDING
+    // ø <SAME AS APPLICATION: update if different>
+    // ø </SAME AS APPLICATION: update if different>
+    // ø </action-actionEvaluation-final>
 }

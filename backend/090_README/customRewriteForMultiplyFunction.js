81ac79f5a085a0a65915140817ac846dc2c0ecd6
ferdely <bradlowry@gmail.com>
2021-10-06 10:35:49 -0500

    commit

--- backend/090_README/customRewriteForMultiplyFunction.js
+++ backend/090_README/customRewriteForMultiplyFunction.js
@@ -17,11 +17,31 @@ export function multiplyFor_BACKEND_FILE_NAME(factor1, factor2) {
     let product =  factor1 * factor2;
     return `In SITE_NAME Backend, in the file 'BACKEND_FILE_NAME', the multiply(${factor1},${factor2}) function return a product of ${product} |${product}| [see session.getItem('lastMultiplyJSON') for YAML to JSON for this backend file]`
 }
+export function gatherYamlFor_BACKEND_FILE_NAME(factor1, factor2) {
+    // return factor1 * factor2;
+	// rewrite function name to: multiplyFor_BACKEND_FILE_NAME
+    // Now Return: Piped-Array `Unique Message|product|reminder of session.getItem()`
+    let thisMultiplyJSON = 'the JSON from the BACKEND FILE FOLDER YAML'
+    // session.setItem('lastMultiplyJSON',thisMultiplyJSON)
+    let product =  factor1 * factor2;
+    return `In SITE_NAME Backend, in the file 'BACKEND_FILE_NAME', the multiply(${factor1},${factor2}) function return a product of ${product} |${product}| [see session.getItem('lastMultiplyJSON') for YAML to JSON for this backend file]`
+}
 // ¡ </REWRITE - Make the function a the Same BUT Unique for each Backend FIle>
 
 
 // ¡ <CALLING FUNCTION - from Canine-Consulting => Import Export Data>
 export async function btnMultiply_click(event) {
+    let uniquePipedString = await multiply($w("#operand1").value, $w("#operand2").value)
+	let responseStringArray = uniquePipedString.split('|')
+	// let product = await multiply($w("#operand1").value, $w("#operand2").value)
+	let product = responseStringArray[1]
+	$w("#product").value = product;
+	let responseStringFinal = typeof session.getItem('lastMultiplyJSON') !== 'string' || (session.getItem('lastMultiplyJSON')).length < 10 ? `Invalid session.getItem('lastMultiplyJSON')` : session.getItem('lastMultiplyJSON');
+	responseStringFinal = `\n==========\n` + responseStringFinal
+	responseStringFinal = responseStringArray[0] + responseStringFinal
+	$w("#multiplyResponseTXAREA").value = responseStringFinal
+	}
+export async function btnGatherYaml_click(event) {
     let uniquePipedString = await multiply($w("#operand1").value, $w("#operand2").value)
 	let responseStringArray = uniquePipedString.split('|')
 	// let product = await multiply($w("#operand1").value, $w("#operand2").value)

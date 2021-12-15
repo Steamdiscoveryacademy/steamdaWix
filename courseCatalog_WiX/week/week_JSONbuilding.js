4ad7ad81a2f5a17252fe614a89c5450e39338d0e
ferdely <bradlowry@gmail.com>
2021-12-15 17:47:43 -0600

    for DocDbJSON=

--- courseCatalog_WiX/week/week_JSONbuilding.js
+++ courseCatalog_WiX/week/week_JSONbuilding.js
@@ -3,25 +3,69 @@
 // ø ================================================================================         <Call Code>
 // ø ================================================================================          (constant)
 // ø ====================================================================================================
- 
+// let YYYY = (new Date()).getFullYear
+// let weekDocDbJSON = composeWeekDocDbJSION(YYYY)
+
 // ø ====================================================================================================
 // ø ================================================================================        </Call Code>
 // ø ====================================================================================================
-let YYYY = (new Date()).getFullYear
-let weekDocDbJSON = composeWeekDocDbJSION(YYYY)
+
+// ø ====================================================================================================
+// ø ======================================================================          <Override Functions>
+// ø ================================================================================          (constant)
+// ø ====================================================================================================
+function composeDocDbJSON_byYear(year){
+    let paramObject = {}
+    let startDate = new Date(year, 0, 1)
+    paramObject.startDate = new Date(year, 11, 31)
+    paramObject.endDate = EndDate
+    return composeWeekDocDbJSON(paramObject)
+}
+function composeDocDbJSON_byTerm(StartDate, EndDate){
+    let paramObject = {}
+    paramObject.startDate = StartDate
+    paramObject.endDate = EndDate
+    return composeWeekDocDbJSON(paramObject)
+}
+// ø ====================================================================================================
+// ø ======================================================================         </Override Functions>
+// ø ====================================================================================================
+
 // ø ====================================================================================================
 // ø ================================================================================     <Function Code>
 // ø ================================================================================          (constant)
 // ø ====================================================================================================
 // ø ====================================================================================================
-function composeWeekDocDbJSION(year){
-    // DO USE Variable Names based on Date-Finess Masking ('01' string vs 1 number etc)
-    let dayCheck = -6 // ¿week will include day 1?
-    let dateStartCheck = new Date(year,month,day)
-    let weekIdCheck = getWeekId(dateStart)
-    let dayStart = 1 
-    let dateStartStart = new Date(year,month,day)
-    let weekIdStart = getWeekId(dateStart)
+function composeWeekDocDbJSON(paramObject = {}){
+    let responseObject = {}
+    responseObject.responseObject = {}
+    responseObject.paramObject = {}
+/**'
+ * NOTES:
+ * • Will respond with all days of included weeks, but where 'dayInSpan' === 0 if need-be
+ * • that is, maybe some pre-days for a Year and maybe some post-days for a Year
+ * • there will (probably) NOT be an override where 'termId' is a parameter
+ * • • this is because this code _calculates_ termId and does not _consume_ termId
+ * EVENTUALLY:
+ * • there will be a check for a Holidy in each Month, it can be marked as Not-Observed
+ * • • this is to confirm that the Holiday-DocDbJSON is valid
+ * • • a Holiday Element is a specific Day, but 'Days Off' can be a range of days
+ * • • • Xmas might be 3 days either side of the Day
+ * • • • July 4th was OBSERVED as July 5th in 2021
+ * • • Thanksgiving is was November 25th 2021 but often you will want to 'take-off' Wed, Thu & Fri
+ * • • Also, since programming can happen on Weekends, 'take-off' is more complicated:
+ * • • • again, with Thanksgiving: you wouldn't want to schedule for the Sat or Sun after as well
+ * • • • in particular STEAMDA has July 4th extend for the entire week
+ * • • • couter-intuitively, STEAMDA may hold programs _on_ a holiday:
+ * • • • • as quasi-day-care for a Holidy here or there that the school observes, but industry does not
+ * ATTRIBUTES:
+ * • weekId: YYYYWW
+ * • termId: YYYYww of start date
+ * • cardinalWeek: 1,2,3... of whatever span
+ * • First Date of Week
+ * • Last Date of Week
+ * • Days of Week (probably [0,1,2,3,4,5,6], but maybe it is different)
+ */
 }
 // ø ====================================================================================================
 // ø ================================================================================     <Function Code>

70f8cd174d1edce7e3c81704f16b5c52cfbe8f96
ferdely <bradlowry@gmail.com>
2022-12-04 06:45:04 -0600

    steamPOSTMAN

---
+++ steamPostman/manualMockServer/exceptionTesting/generalizedMaxChanged/courseMaxChangedPRS.js
@@ -0,0 +1,70 @@
+console.log('<PRS: entering>')
+
+let bradUID = 'braddevel'
+let restUID = 'REST'
+
+let blockUsed = false
+let blockUsedName = 'NNULL'
+let blockSetName = 'BASE'
+blockSetName = 'SPECIFIED_TARGET'
+blockSetName = 'NORMAL_ITERATIVE_STEPPING'
+
+if(blockSetName === 'BASE'){
+    // » <>
+    // » <BASE>
+    // » <BASE Obama>
+    pm.globals.set("courseMaxChanged", "2008-01-20T12:00:00+0000") /* ø NOTE: drupal.changed ISO format*/
+    pm.globals.set("courseMaxChangedGathered", "2022-12-01T23:59:59.001Z") /* ø NOTE: postman {{$isoTimestamp}} ISO format*/
+
+    console.log(`PRS: [${blockSetName}] pm.globals.get("courseMaxChanged"): ${pm.globals.get("courseMaxChanged")}`)
+    // » </BASE Obama>
+    // » </BASE>
+    // » </>
+    blockUsedName = blockSetName
+    blockUsed = true
+}
+
+
+if(blockSetName === 'SPECIFIED_TARGET'){
+    // » <>
+    // » <SPECIFIED_TARGET>
+    // » <SPECIFIED_TARGET> <PRIOR-TO Check-In Difficulties>
+    
+    // pm.globals.set("courseMaxChanged", "2022-11-16T23:59:59+0000")
+    // pm.globals.set("courseMaxChangedGathered", "2022-12-01T23:59:59.002Z")
+
+    pm.globals.set("courseMaxChanged", "2022-11-28T06:37:36+0000")
+    pm.globals.set("courseMaxChangedGathered", "2022-12-03T14:03:19.764Z")
+    
+    console.log(`PRS: [${blockSetName}] pm.globals.get("courseMaxChanged"): ${pm.globals.get("courseMaxChanged")}`)
+    // » </SPECIFIED_TARGET> <PRIOR-TO Check-In Difficulties>
+    // » </SPECIFIED_TARGET>
+    // » </>
+    blockUsedName = blockSetName
+    blockUsed = true
+}
+
+if(blockSetName === 'NORMAL_ITERATIVE_STEPPING'){
+    // » <>
+    // » <NORMAL_ITERATIVE_STEPPING>
+    // » <NORMAL_ITERATIVE_STEPPING> ["maxChanged" as of Previous 'ACTION INDICATED RUN']
+    // » <NORMAL_ITERATIVE_STEPPING> [two lines below direct copy from console.log]
+    pm.globals.set("courseMaxChanged", "2022-11-28T06:37:36+0000")
+    pm.globals.set("courseMaxChangedGathered", "2022-12-03T13:16:07.223Z")
+
+    console.log(`PRS: [${blockSetName}] pm.globals.get("courseMaxChanged"): ${pm.globals.get("courseMaxChanged")}`)
+    // » </NORMAL_ITERATIVE_STEPPING> [two lines below direct copy from console.log]
+    // » </NORMAL_ITERATIVE_STEPPING> ["maxChangedGathered" as Stamp of Previous 'ACTION INDICATED RUN']
+    // » </NORMAL_ITERATIVE_STEPPING>
+    // » </>
+    blockUsedName = blockSetName
+    blockUsed = true
+}
+
+pm.globals.set("blockSetName", blockSetName)
+pm.globals.set("blockUsedName", blockUsedName)
+pm.globals.set("blockUsed", blockUsed)
+
+
+console.log(`PRS:[${blockSetName}] pm.globals.get("courseMaxChanged"): ${pm.globals.get("courseMaxChanged")} (immediately before exiting)`)
+console.log('<PRS: exiting>')
---
+++ steamPostman/manualMockServer/exceptionTesting/generalizedMaxChanged/courseMaxChangedTSTS.js
@@ -0,0 +1,122 @@
+console.log('<TSTS: entering>')
+const maxChangedGatheredPrevious = pm.variables.get("courseMaxChangedGathered")
+console.log(`TSTS: <Actual TESTS - checking if((testCount + failedCount) > 0)>`)
+// » <Actual TESTS - checking if((testCount + failedCount) > 0)>
+let testCount = 0 
+let passedCount = 0 
+let failedCount = 0
+
+testCount++
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+    passedCount++
+    failedCount--
+})
+
+console.log(`Tests Aggregate: Test Count: ${testCount}; Passed Count: ${passedCount}; Failed Count: ${(testCount + failedCount)}`)
+console.log(`if((testCount [${testCount}] + [${failedCount}] failedCount) === 0) » 'CONTINUE'`)
+ 
+// » </Actual TESTS - checking if((testCount + failedCount) > 0)>
+console.log(`TSTS: </Actual TESTS - checking if((testCount + failedCount) > 0)>`)
+
+let maxChangedLoop = pm.variables.get("courseMaxChanged")
+let maxChangedThis = pm.variables.get("courseMaxChanged")
+let maxChangedGatheredThis = pm.variables.replaceIn('{{$isoTimestamp}}')
+
+let responseLogUNIVERSAL = true
+let exhaustiveDocumentation = true
+exhaustiveDocumentation = false
+
+let updated = false
+let index = 0
+let updatedCount = 0
+let updatedTrueLogicArray = []
+let updatedFalseLogicArray = []
+const responseObjectArray = pm.response.json()
+if(exhaustiveDocumentation === true){
+    console.log(`responseObjectArray: [array of course objects below]`)
+    console.log(responseObjectArray)
+}
+
+responseObjectArray.forEach(elementObject => {
+    const changed = elementObject.changed
+    updated = changed > maxChangedLoop ? true : updated
+    updatedCount += changed > maxChangedLoop ? 1 : 0
+    const updateLogicString = `'element[${index}] uid[${elementObject.uid}]: changed [${changed}] > [${maxChangedLoop}] maxChangedLoop'`
+    // ø <EXAUSTIVE-DOCUMENTATION>
+    // ø </EXAUSTIVE-DOCUMENTATION>
+    if(exhaustiveDocumentation === true){
+        // ! <EXAUSTIVE-DOCUMENT>
+        if(changed > maxChangedLoop){
+            updatedTrueLogicArray.push(updateLogicString)
+        }else{
+            updatedFalseLogicArray.push(updateLogicString)
+        }
+        // ! </EXAUSTIVE-DOCUMENT>
+    }
+    // ø </EXAUSTIVE-DOCUMENTATION>
+
+    maxChangedThis = changed > maxChangedLoop ? changed : maxChangedThis
+    index++
+})
+
+// updated = true
+
+if(responseLogUNIVERSAL === true){
+    // ø <TSTS: Universal Response Log>
+    console.log(`
+    <TSTS: Universal Response Log>`)
+    console.log(`PRS: pm.variables.get("blockSetName"): ${pm.variables.get("blockSetName")}`)
+    console.log(`PRS: pm.variables.get("blockUsedName"): ${pm.variables.get("blockUsedName")}`)
+    console.log(`PRS: pm.variables.get("blockUsed"): ${pm.variables.get("blockUsed")}`)
+    console.log(`PRS: pm.variables.get("courseMaxChanged"): ${pm.variables.get("courseMaxChanged")}`)
+    console.log(`PRS: pm.variables.get("courseMaxChangedGathered"): ${pm.variables.get("courseMaxChangedGathered")}`)
+    console.log(`TSTS: maxChangedGatheredPrevious: ${maxChangedGatheredPrevious}`)
+    console.log(`TSTS: maxChangedGatheredThis: ${maxChangedGatheredThis}`)
+    console.log(`TSTS: maxChangedLoop: ${maxChangedLoop}`)
+    console.log(`updated: = ${updated}`)
+    console.log(`updatedCount = ${updatedCount}`)
+    if(exhaustiveDocumentation === true){
+        console.log(`updatedTrueLogicArray = [${updatedTrueLogicArray.toString()}]`)
+        console.log(`updatedFalseLogicArray = [${updatedFalseLogicArray.toString()}]`)
+    }
+    // ! <PRE-TRASH>
+    if(exhaustiveDocumentation === false){
+    // if(exhaustiveDocumentation === 77777){
+        console.log(`updatedTrueLogicArray = [${updatedTrueLogicArray.toString()}]`)
+        console.log(`updatedFalseLogicArray = [${updatedFalseLogicArray.toString()}]`)
+    }
+    // ! </PRE-TRASH>
+    console.log(`TSTS: maxChangedThis: ${maxChangedThis} [dynamic]`)
+    console.log(`TSTS: maxChangedGatheredThis: ${maxChangedGatheredThis} [constant NOW]`)
+    console.log(`</TSTS: Universal Response Log>
+    `)
+    // ø </TSTS: Universal Response Log>
+}
+
+
+
+// pm.variables.get("courseMaxChanged")
+// pm.variables.get("courseMaxChangedGathered")
+// let maxChangedGatheredThis = pm.variables.replaceIn('{{$randomFirstName}}')
+// pm.globals.set("maxChangedGatheredThis", maxChangedGatheredThis)
+
+
+if(updated === true){
+    console.log(`courseMaxChanged Updated (changed) 
+ACTION INDICATED:
+Append the code below to the Pre-request Script:
+copy below FROM HERE =>`)
+    // console.log(`"maxChangedGathered", "${maxChangedGatheredThis}")`)
+    console.log(`pm.globals.set("courseMaxChanged", "${maxChangedThis}")`)
+    console.log(`pm.globals.set("courseMaxChangedGathered", "${maxChangedGatheredThis}")`)
+    console.log(`<= TO HERE`)
+}else{
+console.log(`RESULT:
+No Drupal Course(s) Updated (changed) since ${maxChangedGatheredPrevious}
+NO ACTION INDICATED
+`)
+}
+
+
+console.log('</TSTS: exiting>')

02b3275c1919f8bd771503a77a396c7e647312b4
ferdely <bradlowry@gmail.com>
2022-12-30 11:00:30 -0600

    Picking up Discord Manual Parse Again

---
+++ steamPostman/discord/appPunchClock/manualParse/jsManualParse.js
@@ -0,0 +1,10 @@
+import manualParseArray from '/Users/brad/Documents/bradRepositories/vsCode/steamdaWixLocal/steamdaWix/steamPostman/discord/appPunchClock/manualParse/logs/2022-12-08T15-05-10_manualParse.json'
+
+let Today = "12/08/2022"
+let todayReplaceString = "Today at"
+let Yesterday = "12/07/2022"
+let yesterdayReplaceString = "Yesterday at"
+
+console.warn(`Today: ${Today}`)
+console.warn(`manualParseArray`)
+console.log(JSON.stringify(manualParseArray,undefined,4))
